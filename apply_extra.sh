245e8479c1bd9870097becc4eaad75352aac568f
Evan Anderson <evan@eaanderson.com>
2023-01-24 17:59:55 -0600

    Add InTune support
    * Microsoft InTune is a device management/compliance checker
    that some organizations use for accessing corporate resources.
    When using an InTune enrolled device, Edge talks to the identity
    service using dbus for seamless login to corporate resources, and
    for conditional access enforcement. In order to support this, the
    msalsdk-dbusclient shared library must be present. This commit adds
    this library and its dependency (sd-bus) to the flatpak
    
    * NOTE: This won't work until properly flatpak/xdg-dbus-proxy#21 is fixed

--- apply_extra.sh
+++ apply_extra.sh
@@ -9,4 +9,10 @@ bsdtar -Oxf edge.deb 'data.tar*' |
     ./opt/microsoft/msedge
 rm edge.deb
 
+bsdtar -Oxf msalsdk.deb 'data.tar*' |
+  bsdtar -xf - \
+    --strip-components=3 \
+    ./usr/lib
+rm msalsdk.deb
+
 install -Dm755 /app/bin/stub_sandbox msedge-sandbox
